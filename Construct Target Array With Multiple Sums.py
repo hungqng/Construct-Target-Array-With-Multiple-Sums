ace27ddb14a9d9683496c36be5a83577f87793eb
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-24 00:24:49 -0700

    Add files via upload

--- Construct Target Array With Multiple Sums.py
+++ Construct Target Array With Multiple Sums.py
@@ -40,3 +40,15 @@ class Solution:
             total = new + rest
         return True
 
+        # Solution 3
+        heap = [-num for num in target]
+        total = sum(target)
+        heapify(heap)
+        while heap[0] != -1:
+            num = -heappop(heap)
+            total -= num
+            if num <= total or total < 1: return False
+            num %= total
+            total += num
+            heappush(heap, -num or -total)
+        return True
